a7aeff91d7f707bada7943810afbed864fb1bfdf
Gustavo <110946102+GustavoRez@users.noreply.github.com>
2023-09-22 09:21:05 -0300

    Update SJF.java
    Translating to english

--- Using what I learned/SJF.java
+++ Using what I learned/SJF.java
@@ -3,24 +3,24 @@ import javax.swing.JOptionPane;
 public class SJF {
     public static void main (String[] args){
         
-    int Qtd_P, Gamb = 0;
+    int Qt_P, aux = 0;
 
-    Qtd_P = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantos processos?"));
+    Qtd_P = Integer.parseInt(JOptionPane.showInputDialog(null, "How many process?"));
 
-    int Pro[] = new int[Qtd_P];
-    int Pro2[] = new int[Qtd_P];
+    int Pro[] = new int[Qt_P];
+    int Pro2[] = new int[Qt_P];
 
-    for (int i = 0; i < Qtd_P; i++){
-        Pro[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual o tamanho do processo " + (i + 1) + " em ms?"));
+    for (int i = 0; i < Qt_P; i++){
+        Pro[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "How long is the process " + (i + 1) + " in ms?"));
         Pro2[i] = Pro[i];
     }
 
     for(int i = 0; i < Pro.length; i++){
 		for(int j = 0; j < Pro.length; j++){
 			if(Pro[i] < Pro[j]){
-				Gamb = Pro[i];
+				aux = Pro[i];
 				Pro[i] = Pro[j];
-				Pro[j] = Gamb;
+				Pro[j] = aux;
             }
         }
     }
@@ -28,7 +28,7 @@ public class SJF {
     for (int i = 0; i < Pro.length; i++){
         for (int j = 0; j < Pro.length; j++){
             if (Pro2[j] == Pro[i])
-                JOptionPane.showMessageDialog(null, "Processo " + (j + 1) + "\n" + Pro[i] + "ms...", "Executando...", 1);
+                JOptionPane.showMessageDialog(null, "Process " + (j + 1) + "\n" + Pro[i] + "ms...", "Executing...", 1);
         }
     }
     }
